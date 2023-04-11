1d63dd85d7b832de90736080d0434ede254e83d0
kennywestt <dlrudrms0617@naver.com>
2023-04-11 13:23:03 +0900

    경근 record,place 화면전환

--- app/src/main/java/com/example/easypizy/MainActivity.kt
+++ app/src/main/java/com/example/easypizy/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.easypizy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.easypizy.databinding.ActivityMainBinding
+import com.example.easypizy.presentation.view_model.Home
+import com.example.easypizy.presentation.view_model.Page
+import com.example.easypizy.presentation.view_model.Map
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+
+
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home-> replaceFragment(Home())
+                R.id.map-> replaceFragment(Map())
+                R.id.page-> replaceFragment(Page())
+
+                else->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
     }
 }
+
