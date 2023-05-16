0ebc7fcb5b24d0276dc1ec7c9d721e18cb344f19
Seolong <86753626+seolong@users.noreply.github.com>
2023-05-16 20:20:49 +0900

    경근 메인화면 최종 변경1

--- app/src/main/java/com/example/easypizy/MyApp.kt
+++ app/src/main/java/com/example/easypizy/MyApp.kt
@@ -1,12 +1,23 @@
 package com.example.easypizy
 
 import android.app.Application
+import android.content.Context
 import com.example.easypizy.data.data_source.local.SmokeMemoDatabase
 import com.example.easypizy.data.data_source.local.SmokePlaceDatabase
 import com.example.easypizy.data.repository.SmokeMemoRepository
 import com.example.easypizy.data.repository.SmokePlaceRepository
 
-class SmokePlaceApplication : Application() {
+class MyApp : Application() {
+    lateinit var context: Context
+    init {
+        instance =this}
+    companion object {
+        private var instance: MyApp? = null
+        fun applicationContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
     val smokePlaceDatabase by lazy { SmokePlaceDatabase.getDatabase(this) }
     val smokePlaceRepository by lazy { SmokePlaceRepository(smokePlaceDatabase.smokePlaceDao()) }
 
--- app/src/main/java/com/example/easypizy/data/data_source/remote/SmokeApi.kt
+++ app/src/main/java/com/example/easypizy/data/data_source/remote/SmokeApi.kt
@@ -1,12 +1,12 @@
 package com.example.easypizy.data.data_source.remote
 
-import com.example.easypizy.data.data_source.remote.dto.smoke_place.GwangJinDtoList
+import com.example.easypizy.data.data_source.remote.dto.smoke_place.SmokeDto
 import retrofit2.Call
 import retrofit2.http.GET
 
-private const val apiKey: String = "iqsfQNfPIs8JsmMOXxBztm%2B4GmUT%2BqCgf7kQkc0xabgp6dnCOm4Z0o1YISAU%2Bg%2BlApxUu4KznQ8msFhW6jAEJQ%3D%3D"
-
 interface SmokeApi {
-    @GET("getSmkAreaList?type=json&serviceKey=$apiKey&pageNo=1&numOfRows=500&id=")
-    fun getAllSmokePlaces(): Call<GwangJinDtoList>
+    @GET("/v3/19636cc1-f10d-45b6-8ea0-8a52a423d117")
+    fun getAllSmoke(): Call<SmokeDto>
+
+
 }
